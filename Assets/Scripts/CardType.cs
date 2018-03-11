794dc8906ecc0dac344fc814a7d3ed602140ddc6
Clinton Olsen <kadaj8899@yahoo.com>
2018-03-11 16:21:04 -0600

    Added CardTypes enum.

---
+++ Assets/Scripts/CardType.cs
@@ -0,0 +1,18 @@
+﻿public enum CardType
+{
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Wild,
+    WildDrawFour,
+    DrawTwo,
+    Skip,
+    Reverse
+}
