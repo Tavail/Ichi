b196691126a987159131d96003b35c41eee23897
Wink2424 <wyattolsen@github.com>
2018-03-11 17:27:56 -0600

    Added Card Object

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Assets.Scripts.Types;
+
+public class Card
+{
+    private Guid iD;
+    private CardType cardType;
+    private Color colorType;
+
+    public Card(CardType card, Color color)
+    {
+        iD = new Guid();
+        cardType = card;
+        colorType = color;
+    }
+
+    public CardType CardType
+    {
+        get
+        {
+            return cardType;
+        }
+    }
+     
+    public Color Color
+    {
+        get
+        {
+            return colorType;
+        }
+    }
+}
+
